5c036d4c7ef4c7dd87b87802b2f97c22dd79185a
Antonis Sfikakis <sdi2200178@di.uoa.gr>
2026-04-02 17:48:31 +0300

    Cup implementation

--- Q2/script.sh
+++ Q2/script.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-rm -rf Scanner*;
+rm -rf Scanner* output.java Output.java Output.class
+java -jar /usr/share/java-cup-11b.jar -parser Parser -symbols sym -expect 3 parser.cup
 jflex scanner.flex
-javac Scanner.java
-java Scanner test.txt
-
+javac -cp .:/usr/share/java-cup-11b-runtime.jar *.java
+java -cp .:/usr/share/java-cup-11b-runtime.jar Main < test.txt > Output.java
+javac Output.java
+java Output
---
+++ Q2/src/Main.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.io.IOException;
+
+public class Main { 
+  public static void main(String[] args) throws Exception {
+        Parser p = new Parser(new Scanner(new InputStreamReader(System.in)));
+        p.parse();
+
+  }
+
+
+}
